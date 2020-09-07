49c7eec3ea70ff886b107c39e5d7899867699d21
Jake Waggoner <waggonerjake99@gmail.com>
2020-09-06 20:17:58 -0400

    Run chrome in the background and use a different log in route

--- bot.rb
+++ bot.rb
@@ -6,20 +6,29 @@ username = config['username']
 password = config['password']
 tweet = config['tweet']
 
-driver = Selenium::WebDriver.for :chrome
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument("window-size=1920,1080")
+options.add_argument("start-maximized")
+options.add_argument("headless")
+driver = Selenium::WebDriver.for(:chrome, options: options)
 driver.navigate.to "https://www.twitter.com/"
 
 sleep(5)
 
-element = driver.find_element(xpath: '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div[1]/div/form/div/div[1]/div/label/div/div[2]/div/input')
+element = driver.find_element(xpath: '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div[2]/a[2]')
+element.click
+
+sleep(5)
+
+element = driver.find_element(xpath: '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
 element.click
 element.send_keys username
 
-element = driver.find_element(xpath: '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div[1]/div/form/div/div[2]/div/label/div/div[2]/div/input')
+element = driver.find_element(xpath: '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
 element.click
 element.send_keys password
 
-element = driver.find_element(xpath: '//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div[1]/div/form/div/div[3]/div')
+element = driver.find_element(xpath: '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div')
 element.click
 
 sleep(5)
